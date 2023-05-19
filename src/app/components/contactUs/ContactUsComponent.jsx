02a13f6649f0f51cb15f88bce2d9abebe1445413
Anno <annogomes@gmail.com>
2023-05-19 16:36:34 +1000

    update contact page design and mobile friendly

--- src/app/components/contactUs/ContactUsComponent.jsx
+++ src/app/components/contactUs/ContactUsComponent.jsx
@@ -1,143 +1,153 @@
 import React from "react";
 import { Grid, TextField, Button, Typography } from "@mui/material";
-
 import "./ContactUs.css";
 import "@fontsource/bodoni-moda";
-
+import { Box } from '@mui/system';
 import { withStyles } from "@material-ui/core/styles";
 
-export default function ContactUsComponent() {
-  const CssTextField = withStyles({
+const CssTextField = withStyles({
     root: {
-      "& label.Mui-focused": {
-        color: "white",
-      },
-      "& .MuiInput-underline:after": {
-        borderBottomColor: "yellow",
-      },
-      "& .MuiOutlinedInput-root": {
-        "& fieldset": {
-          borderColor: "white",
+        '& .MuiInputBase-root': {
+            backgroundColor: '#D9D9D9', 
+            borderRadius: '20px', 
         },
-        "&:hover fieldset": {
-          borderColor: "white",
+        "& label": {
+            color: "#370E4A", 
         },
-        "&.Mui-focused fieldset": {
-          borderColor: "yellow",
+        "& .MuiOutlinedInput-root": {
+            '&.Mui-focused fieldset': {
+                borderColor: "#370E4A", 
+            },
         },
-      },
+        '& .MuiOutlinedInput-notchedOutline': {
+            borderColor: 'transparent', 
+        },
+        '&:hover .MuiOutlinedInput-notchedOutline': {
+            borderColor: 'transparent', 
+        },
+        "& input": {
+            color: "#000000", 
+        },
     },
-  })(TextField);
+    input: {
+        '&::placeholder': {
+            textOverflow: 'ellipsis !important',
+            color: '#B2B2A2' 
+        }
+    }
+})(TextField);
 
+export default function ContactUsComponent() {
   return (
     <div className="contactUs-center">
-      <Grid container>
-        <Grid item style={{ flexGrow: "1" }}>
+      <Grid container direction="column" alignItems="center">
+        <Grid item xs={12} sm={6}>
           <Typography
             gutterBottom
             variant="h4"
             style={{
               fontFamily: "Roboto",
-              fontSize: "40",
-              color: "#FFFFFF",
+              fontSize: "90px",
+              color: "#E9624C",
+              textAlign: "center"
             }}
           >
-            Contact Us
+            CONTACT US
           </Typography>
           <Typography
             gutterBottom
             variant="h6"
-            style={{ marginTop: "40px", color: "#FFFFFF" }}
+            style={{ marginTop: "10px", color: "#fff", textAlign: "center", fontSize: "46px" }}
           >
             Redback Operation
           </Typography>
           <Typography
             gutterBottom
             variant="h6"
-            style={{ marginTop: "40px", color: "#FFFFFF" }}
+            style={{ marginTop: "10px", color: "#fff", textAlign: "center", fontSize: "30px" }}
           >
-            admin@mail.com
+            E-mail - <a href="mailto:admin@mail.com">admin@mail.com</a>
           </Typography>
-        </Grid>
-        <Grid xs={6} item>
-          <form>
-            <Grid container spacing={1}>
-              <Grid xs={12} sm={12} item>
-                <CssTextField
-                  sx={{ "& label": { color: "white" } }}
-                  inputProps={{
-                    style: { color: "white" },
-                  }}
-                  placeholder="Enter your name"
-                  label="Name"
-                  variant="outlined"
-                  fullWidth
-                  required
-                />
+          <Box
+            sx={{
+              borderRadius: '40px',
+              border: '2px solid #D6594C',
+              p: 3,
+              mt: 2,
+              mb: 2,
+              bgcolor: '#D6594C',
+              width: '100%',
+            }}
+          >
+            <form>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <CssTextField
+                    placeholder="Enter your name"
+                    label="Name"
+                    variant="outlined"
+                    fullWidth
+                    required
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <CssTextField
+                    type="email"
+                    placeholder="Enter email"
+                    label="Email"
+                    variant="outlined"
+                    fullWidth
+                    required
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <CssTextField
+                    type="number"
+                    placeholder="Enter phone number"
+                    label="Phone"
+                    variant="outlined"
+                    fullWidth
+                    required
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <CssTextField
+                    label="Message"
+                    multiline
+                    rows={4}
+                    placeholder="Type your message here"
+                    variant="outlined"
+                    fullWidth
+                    required
+                  />
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <CssTextField
-                  sx={{ "& label": { color: "white" } }}
-                  inputProps={{
-                    style: { color: "white" },
-                  }}
-                  style={{ borderColor: "#62656A" }}
-                  type="email"
-                  placeholder="Enter email"
-                  label="Email"
-                  variant="outlined"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <CssTextField
-                  sx={{ "& label": { color: "white" } }}
-                  inputProps={{
-                    style: { color: "white" },
-                  }}
-                  type="number"
-                  placeholder="Enter phone number"
-                  label="Phone"
-                  variant="outlined"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <CssTextField
-                  style={{ color: "white" }}
-                  sx={{ "& label": { color: "white" } }}
-                  inputProps={{
-                    style: { color: "white" },
-                  }}
-                  label="Message"
-                  multiline
-                  rows={4}
-                  placeholder="Type your message here"
-                  variant="outlined"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid item xs={3}>
-                <Button
-                  style={{
-                    borderRadius: 0,
-                    backgroundColor: "white",
-                    color: "black",
-                    margin: "5px",
-                  }}
-                  type="submit"
-                  variant="contained"
-                  color="primary"
-                  fullWidth
-                >
-                  Submit
-                </Button>
-              </Grid>
-            </Grid>
-          </form>
+            </form>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              mt: 2
+            }}
+          >
+            <Button
+              style={{
+                borderRadius: '20px',
+                backgroundColor: "#5c2a91",
+                color: "white",
+                textTransform: 'uppercase',
+                padding: '10px 20px',
+                width: '236px',
+                marginBottom: '40px'
+              }}
+              type="submit"
+              variant="contained"
+              color="primary"
+            >
+              Submit
+            </Button>
+          </Box>
         </Grid>
       </Grid>
     </div>
